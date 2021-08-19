790ca9caf69315e2c28ecbc0a08327e2ee5453b1
Carlos Eduardo Arismendi Sanchez <carlos.arismendisanchez@hotmail.com>
2021-08-19 12:21:16 +0200

    Added 'New Game' button

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,6 @@
 let board = null
 
-window.onload = function() {
-  init()
-}
-
-const BOARD_DIMENSION = 80 //vmin
-const CELL_SIZE = BOARD_DIMENSION / 8; //px
+window.onload = init
 
 function init () {
   board = new Board({ selector: '#board' })
