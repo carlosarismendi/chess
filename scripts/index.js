5d6c688bffa7c907c7a30754a6ab98d650ff83cc
Carlos Eduardo Arismendi Sanchez <carlos.arismendisanchez@hotmail.com>
2021-08-26 11:27:31 +0200

    Puzzle in fen notation in index.js for testing check movements

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,10 @@ let game = null
 window.onload = init
 
 function init () {
-  game = new Game({ selector: '#board', fen_string: INITIAL_POSITION_FEN })
+  // game = new Game({ selector: '#board', fen_string: INITIAL_POSITION_FEN })
+
+  fen_string = "3r2kb/4q2p/p3p1pQ/1p4N1/2n2RP1/4P2P/KP1r4/3N1R2 w -- - 0 1"
+  game = new Game({ selector: '#board', fen_string: fen_string })
 }
 
 function newGame () {
