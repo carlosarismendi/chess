b4127b365a6bd4497feae1d57462b26d7fb65862
Carlos Eduardo Arismendi Sanchez <carlos.arismendisanchez@hotmail.com>
2021-08-26 12:17:04 +0200

    Another check position for testing in fen_notation

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,8 @@ window.onload = init
 function init () {
   // game = new Game({ selector: '#board', fen_string: INITIAL_POSITION_FEN })
 
-  fen_string = "3r2kb/4q2p/p3p1pQ/1p4N1/2n2RP1/4P2P/KP1r4/3N1R2 w -- - 0 1"
+  // fen_string = "3r2kb/4q2p/p3p1pQ/1p4N1/2n2RP1/4P2P/KP1r4/3N1R2 w -- - 0 1"
+  fen_string = "8/1k5p/8/4r3/4R3/5Q2/8/8 w -- - 0 1"
   game = new Game({ selector: '#board', fen_string: fen_string })
 }
 
